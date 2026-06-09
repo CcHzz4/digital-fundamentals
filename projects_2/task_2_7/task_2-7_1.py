198f6e5ad4b6d123cc17c8e822f5ab556782ee73
Виктория <vika2007deva@yandex.ru>
2026-06-09 16:25:11 +0300

    Update task_2-7_1.py

--- projects_2/task_2_7/task_2-7_1.py
+++ projects_2/task_2_7/task_2-7_1.py
@@ -1,7 +1,6 @@
 files = ["seq1", "seq2", "seq3", "seq4"]
+date = "2004-04-20"
 
 for name in files:
-
-   new_name = name + ".fasta"+ ".05.04.26"
-
-   print(f"{new_name}")
+    new_name = name + "_" + date + ".fasta"
+    print(new_name)
