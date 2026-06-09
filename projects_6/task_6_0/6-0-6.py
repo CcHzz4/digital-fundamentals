9e942708281c1317e18495ce66ad42924c18c4a4
Виктория <vika2007deva@yandex.ru>
2026-06-09 11:51:17 +0300

    Update 6-0-6.py

--- projects_6/task_6_0/6-0-6.py
+++ projects_6/task_6_0/6-0-6.py
@@ -1,8 +1,9 @@
 import pandas as pd
 with open("C:/Users/Виктория/Desktop/6/6_0_6.txt", "w") as f:
     df = pd.read_csv("C:/Users/Виктория/Downloads/wild_boars.csv")
-    q1 = df.groupby('gender')['length_cm'].quantile(0.25)
+    q1 = df.groupby('gender')['length_cm'].quantile(0.25) #групп по уник значеням м ж и проценталь
     q3 = df.groupby('gender')['length_cm'].quantile(0.75)
     iqr = q3-q1
     for gender in iqr.index: #автоматичски создастся индекс из-за групбай, те тут это мэйл фемэйл
         print(f"{gender}\t iqr = {iqr[gender]:.2f} cm", file=f)
+#размах по длине для м и в отдельный файл, дисперсия(уд от их ср ариф)
